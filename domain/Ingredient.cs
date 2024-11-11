adb54d38f812d3427b1d6634b7bd739e393a96de
Pedro Hurtado <pedrohurt@gmail.com>
2024-11-11 15:56:26 +0100

    first commit

---
+++ domain/Ingredient.cs
@@ -0,0 +1,20 @@
+namespace webapi.domainpizza{
+    public class Ingredient{
+        public Guid Id {get;private set;}
+        public string Name {get;private set;}
+        public decimal Cost {get;private set;}
+
+        protected Ingredient(Guid id, string name, decimal cost){
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+        public void Update(string name, decimal cost){
+            Name = name;
+            Cost = cost;
+        }
+        public Ingredient Create(string name, decimal cost){
+            return new Ingredient(Guid.NewGuid(), name,cost);            
+        }
+    }
+}
